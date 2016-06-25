c86499da027f2482ca17d2956af5316c4830538d
RowleyCowper <rowland.marshall@gmail.com>
2016-06-25 13:32:51 +0200

    Added concept and work package plan for histogram compare
    root directory contains concept plan and work package description
    
    Other directory contains loose end trials and outputs from QGIS
    experiments

---
+++ Field Compare/Working Directory/AppRank/simple.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode Tkinter tutorial
+
+This script shows a simple window
+on the screen.
+
+Author: Jan Bodnar
+Last modified: November 2015
+Website: www.zetcode.com
+"""
+
+from Tkinter import Tk, Frame, BOTH
+
+
+class Example(Frame):
+  
+    def __init__(self, parent):
+        Frame.__init__(self, parent, background="white")   
+         
+        self.parent = parent
+        
+        self.initUI()
+        
+    
+    def initUI(self):
+      
+        self.parent.title("Simple")
+        self.pack(fill=BOTH, expand=1)
+        
+
+def main():
+  
+    root = Tk()
+    root.geometry("250x150+300+300")
+    app = Example(root)
+    root.mainloop()  
+
+
+if __name__ == '__main__':
+    main()
